a0170ab0903fc6582fc4c99fdeb184279159b4c3
Harkirat <hs3188248@gmail.com>
2023-04-05 05:21:27 +0530

    conditionals

---
+++ Project/js/conditionals.js
@@ -0,0 +1,39 @@
+// Variables in js 
+
+let name = "Harkirat";
+let age = 40;
+
+
+if(name=="surya" || age <25 ){
+console.log("He is a student")
+}
+else if(name == "Harkirat" && age>25){
+console.log("He is a teacher")
+}
+else{
+    console.log("Name not recognised")
+}
+
+
+// and operator and or operator
+// And -  It says strictly all the conditions shall be true only it is going to proceed (&&)
+// ==> if both condition are true
+// Or - It says either on of all condition should be true , it is going to proceed . (||)
+// ==> Either of the condition should work 
+
+// -------------------------------------- Program ----------------------------------
+// Write a program to tell the user if he can win the game today ? 
+
+// User is going to enter the his mood , the weather 
+
+// 1. if mood is happy and weather is sunny then user is going to win 
+// 2. if mood is sad and weather is sunny then user is going to lose.
+// 3. if mood id happy and weather is rainy then user is going to lose.
+// 4. if user id sad and weather is sunny then aslo user id going to win.
+
+
+let mood = "happy"
+let weather = "sunny"
+
+
+
